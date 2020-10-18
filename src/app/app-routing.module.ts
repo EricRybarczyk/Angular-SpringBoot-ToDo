9b22a9c9b04be9f78bc2ac1af27a56a426eddee7
EricRybarczyk <EricRybarczyk@gmail.com>
2020-10-18 15:39:37 -0400

    Removed the Angular-generated site content I had previously moved to the ngInfo component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {WelcomeComponent} from './welcome/welcome.component';
 import {ErrorComponent} from './error/error.component';
-import {NginfoComponent} from './nginfo/nginfo.component';
 import {ListTodosComponent} from './list-todos/list-todos.component';
 import {LogoutComponent} from './logout/logout.component';
 import {RouteGuardService} from './service/route-guard.service';
@@ -12,7 +11,6 @@ const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'welcome/:name', component: WelcomeComponent, canActivate: [RouteGuardService] },
-  { path: 'info', component: NginfoComponent },
   { path: 'todos', component: ListTodosComponent, canActivate: [RouteGuardService] },
   { path: 'logout', component: LogoutComponent },
   { path: '**', component: ErrorComponent }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
-import {FormsModule} from '@angular/forms';
-import { NginfoComponent } from './nginfo/nginfo.component';
+import { FormsModule } from '@angular/forms';
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
 import { MenuComponent } from './menu/menu.component';
@@ -18,7 +17,6 @@ import { LogoutComponent } from './logout/logout.component';
     AppComponent,
     WelcomeComponent,
     LoginComponent,
-    NginfoComponent,
     ErrorComponent,
     ListTodosComponent,
     MenuComponent,
